5f8e2206abf6e8855d63e76a950b6d1500e7e556
Jozandro23 <lizandroht2002@gmail.com>
2023-08-01 20:58:23 -0600

    Se agregó UsuariosService y UsuariosTO

---
+++ src/Clases/UsariosService.java
@@ -0,0 +1,136 @@
+
+package Clases;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author javi
+ */
+public class UsariosService extends Connector {
+
+    public void insert(UsuariosTO usuario) throws Exception {
+
+        Connection conn = getConnection();
+        PreparedStatement ps = conn.prepareStatement("INSERT INTO BIBLIOTECA.USUARIO VALUES(?,?,?,?,?,?)");
+        ps.setInt(1, 0);
+        ps.setString(2, usuario.getNombre());
+        ps.setString(3, usuario.getDireccion());
+        ps.setInt(4, usuario.getTelefono());
+        ps.setString(4, usuario.getCiudad());
+        ps.setString(4, usuario.getCorreo());
+
+        ps.executeUpdate();
+        close(ps);
+        close(conn);
+    }
+
+
+    public void delete(int id) throws Exception {
+        Connection conn = getConnection();
+        PreparedStatement ps = null;
+
+        ps = getConn().prepareStatement("DELETE FROM BIBLIOTECA.USUARIO WHERE ID_USUARIO=?");
+        ps.setInt(1, id);
+
+        ps.executeUpdate();
+
+        super.close(ps);
+        super.close(conn);
+
+    }
+
+    public void update(int id, String nombre, String direccion, int telefono, String ciudad, String correo) throws Exception {
+        Connection conn = getConnection();
+        PreparedStatement ps = conn.prepareStatement("UPDATE BIBLIOTECA.USUARIO SET NOMBRE = ?, DIRECCION = ?, TELEFONO = ?, CIUDAD = ?, CORREO = ? WHERE ID_USUARIO = ?");
+        ps.setString(1, nombre);
+        ps.setString(2, direccion);
+        ps.setInt(3, telefono);
+        ps.setString(4, ciudad);
+        ps.setString(5, correo);
+        ps.setInt(6, id);
+        ps.executeUpdate();
+        close(ps);
+        close(conn);
+        close(conn);
+    }
+
+    
+
+    public List<UsuariosTO> getUsuario() throws Exception {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Connection conn = getConnection();
+        List<UsuariosTO> retorno = new ArrayList<UsuariosTO>();
+
+        ps = getConn().prepareStatement("SELECT * FROM BIBLIOTECA.USUARIO");
+        rs = ps.executeQuery();
+        while (rs.next()) {
+            UsuariosTO usuariosTO;
+            int id = rs.getInt("id_usuario");
+            String nombre = rs.getString("nombre");
+            String direccion = rs.getString("direccion");
+            int telefono = rs.getInt("telefono");
+            String ciudad = rs.getString("ciudad");
+            String correo = rs.getString("correo");
+
+            usuariosTO = new UsuariosTO(id, nombre, direccion, telefono, ciudad, correo);
+            //String tblData[] = {titulo, anio, editorial};
+
+            retorno.add(usuariosTO);
+
+        }
+
+        super.close(rs);
+        super.close(ps);
+        super.close(conn);
+
+        return retorno;
+    }
+
+    public void RellenaLaTablaConDatosMySQL(String tabla, JTable visor) {
+        try {
+            String sql = "Select * from " + tabla;
+            Statement st;
+            Connection conn = getConnection();
+            DefaultTableModel model = new DefaultTableModel();
+            model.addColumn("ID Libro");
+            model.addColumn("Nombre");
+            model.addColumn("Direccion");
+            model.addColumn("Telefono");
+            model.addColumn("Ciudad");
+            model.addColumn("Correo");
+
+            visor.setModel(model);
+            String[] dato = new String[7];
+            try {
+                st = conn.createStatement();
+                ResultSet rs = st.executeQuery(sql);
+                while (rs.next()) {
+                    dato[0] = rs.getString(1);
+                    dato[1] = rs.getString(2);
+                    dato[2] = rs.getString(3);
+                    dato[3] = rs.getString(4);
+                    dato[4] = rs.getString(5);
+                    dato[5] = rs.getString(6);
+                    model.addRow(dato);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        } catch (Exception e) {
+
+        }
+
+    }
+}
---
+++ src/Clases/UsuariosTO.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author lizan
+ */
+public class UsuariosTO {
+    
+    private int id;
+    private String nombre;
+    private String direccion;
+    private int telefono;
+    private String ciudad;
+    private String correo;
+
+    public UsuariosTO() {
+    }
+
+    public UsuariosTO(int id, String nombre, String direccion, int telefono, String ciudad, String correo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.ciudad = ciudad;
+        this.correo = correo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    
+    
+}
