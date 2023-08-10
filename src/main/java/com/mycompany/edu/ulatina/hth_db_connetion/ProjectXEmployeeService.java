86f9fd0184b22c0ca77a63f70f282993fc4b6149
Nerkonist <128267548+Nerkonist@users.noreply.github.com>
2023-08-09 18:39:50 -0600

    Se modificó projectxemployee

--- src/main/java/com/mycompany/edu/ulatina/hth_db_connetion/ProjectXEmployeeService.java
+++ src/main/java/com/mycompany/edu/ulatina/hth_db_connetion/ProjectXEmployeeService.java
@@ -12,12 +12,10 @@ public class ProjectXEmployeeService extends Service implements ICrud<ProjectXEm
     @Override
     public void insert(ProjectXEmployeeTO projectXEmployeeTO) throws Exception {
         Connection conn = getConnection();
-        PreparedStatement ps = conn.prepareStatement("INSERT INTO HTH.PROJECT_X_EMPLOYEE VALUES(?,?,?,?,?)");
+        PreparedStatement ps = conn.prepareStatement("INSERT INTO HTH.PROJECT_X_EMPLOYEE VALUES(?,?,?)");
         ps.setInt(1, 0);
         ps.setInt(2, projectXEmployeeTO.getIdProject());
         ps.setInt(3, projectXEmployeeTO.getIdEmployee());
-        ps.setInt(4, projectXEmployeeTO.getHoursInvested());
-        ps.setString(5, projectXEmployeeTO.getFeedBack());
         ps.executeUpdate();
         close(ps);
         close(conn);
@@ -25,12 +23,10 @@ public class ProjectXEmployeeService extends Service implements ICrud<ProjectXEm
 
     public void insert(int idProject, int idEmployee, int hoursInvested, String feedBack) throws Exception {
         Connection conn = getConnection();
-        PreparedStatement ps = conn.prepareStatement("INSERT INTO HTH.PROJECT_X_EMPLOYEE VALUES(?,?,?,?,?)");
+        PreparedStatement ps = conn.prepareStatement("INSERT INTO HTH.PROJECT_X_EMPLOYEE VALUES(?,?,?)");
         ps.setInt(1, 0);
         ps.setInt(2, idProject);
         ps.setInt(3, idEmployee);
-        ps.setInt(4, hoursInvested);
-        ps.setString(5, feedBack);
         ps.executeUpdate();
         close(ps);
         close(conn);
@@ -57,12 +53,10 @@ public class ProjectXEmployeeService extends Service implements ICrud<ProjectXEm
 
     public void update(ProjectXEmployeeTO projectXEmployeeTO) throws Exception {
         Connection conn = getConnection();
-        PreparedStatement ps = conn.prepareStatement("UPDATE HTH.PROJECT_X_EMPLOYEE SET ID_PROJECT = ?, ID_EMPLOYEE = ?, HOURS_INVESTED = ?, FEEDBACK = ? WHERE ID = ?");
+        PreparedStatement ps = conn.prepareStatement("UPDATE HTH.PROJECT_X_EMPLOYEE SET ID_PROJECT = ?, ID_EMPLOYEE = ? WHERE ID = ?");
         ps.setInt(1, projectXEmployeeTO.getIdProject());
         ps.setInt(2, projectXEmployeeTO.getIdEmployee());
-        ps.setInt(3, projectXEmployeeTO.getHoursInvested());
-        ps.setString(4, projectXEmployeeTO.getFeedBack());
-        ps.setInt(5, projectXEmployeeTO.getId());
+        ps.setInt(3, projectXEmployeeTO.getId());
         ps.executeUpdate();
         close(ps);
         close(conn);
@@ -70,12 +64,10 @@ public class ProjectXEmployeeService extends Service implements ICrud<ProjectXEm
 
     public void update(ProjectXEmployeeTO projectXEmployeeTO, int newIdProject, int newIdEmployee, int newHoursInvested, String newFeedback) throws Exception {
         Connection conn = getConnection();
-        PreparedStatement ps = conn.prepareStatement("UPDATE HTH.PROJECT_X_EMPLOYEE SET ID_PROJECT = ?, ID_EMPLOYEE = ?, HOURS_INVESTED = ?, FEEDBACK = ? WHERE ID = ?");
+        PreparedStatement ps = conn.prepareStatement("UPDATE HTH.PROJECT_X_EMPLOYEE SET ID_PROJECT = ?, ID_EMPLOYEE = ? WHERE ID = ?");
         ps.setInt(1, newIdProject);
         ps.setInt(2, newIdEmployee);
-        ps.setInt(3, newHoursInvested);
-        ps.setString(4, newFeedback);
-        ps.setInt(5, projectXEmployeeTO.getId());
+        ps.setInt(3, projectXEmployeeTO.getId());
         ps.executeUpdate();
         close(ps);
         close(conn);
@@ -84,15 +76,13 @@ public class ProjectXEmployeeService extends Service implements ICrud<ProjectXEm
     public List<ProjectXEmployeeTO> getProjectXEmployeeInfo() throws Exception {
         Connection conn = getConnection();
         List<ProjectXEmployeeTO> projectXEmployeeList = new ArrayList<>();
-        PreparedStatement ps = conn.prepareStatement("SELECT ID,ID_PROJECT,ID_EMPLOYEE,HOURS_INVESTED,FEEDBACK FROM HTH.PROJECT_X_EMPLOYEE");
+        PreparedStatement ps = conn.prepareStatement("SELECT ID,ID_PROJECT,ID_EMPLOYEE FROM HTH.PROJECT_X_EMPLOYEE");
         ResultSet rs = ps.executeQuery();
         while (rs.next()) {
             int id = rs.getInt("id");
             int idProject = rs.getInt("ID_PROJECT");
             int idEmployee = rs.getInt("ID_EMPLOYEE");
-            int hoursInvested = rs.getInt("HOURS_INVESTED");
-            String feedBack = rs.getString("FEEDBACK");
-            ProjectXEmployeeTO projectXEmployeeTO = new ProjectXEmployeeTO(id, idProject, idEmployee, hoursInvested, feedBack);
+            ProjectXEmployeeTO projectXEmployeeTO = new ProjectXEmployeeTO(id, idProject, idEmployee);
             projectXEmployeeList.add(projectXEmployeeTO);
         }
         close(rs);
@@ -104,7 +94,7 @@ public class ProjectXEmployeeService extends Service implements ICrud<ProjectXEm
     public List<ProjectXEmployeeTO> getPXEInfoFrom(int pk) throws Exception {
         Connection conn = getConnection();
         List<ProjectXEmployeeTO> projectXEmployeeList = new ArrayList<>();
-        PreparedStatement ps = conn.prepareStatement("SELECT ID,ID_PROJECT,ID_EMPLOYEE,HOURS_INVESTED,FEEDBACK FROM HTH.PROJECT_X_EMPLOYEE WHERE ID = ?");
+        PreparedStatement ps = conn.prepareStatement("SELECT ID,ID_PROJECT,ID_EMPLOYEE FROM HTH.PROJECT_X_EMPLOYEE WHERE ID = ?");
         ps.setInt(1, pk);
         ResultSet rs = ps.executeQuery();
 
@@ -112,10 +102,8 @@ public class ProjectXEmployeeService extends Service implements ICrud<ProjectXEm
             int id = rs.getInt("ID");
             int idProject = rs.getInt("ID_PROJECT");
             int idEmployee = rs.getInt("ID_EMPLOYEE");
-            int hoursInvested = rs.getInt("HOURS_INVESTED");
-            String feedBack = rs.getString("FEEDBACK");
 
-            ProjectXEmployeeTO projectEmployeeTO = new ProjectXEmployeeTO(id, idProject, idEmployee, hoursInvested, feedBack);
+            ProjectXEmployeeTO projectEmployeeTO = new ProjectXEmployeeTO(id, idProject, idEmployee);
             projectXEmployeeList.add(projectEmployeeTO);
         }
 
@@ -126,30 +114,17 @@ public class ProjectXEmployeeService extends Service implements ICrud<ProjectXEm
         return projectXEmployeeList;
     }
 
-    public void addFeedbackToEmployee(int idProject, int idEmployee, String feedBack) throws Exception {
-        Connection conn = getConnection();
-        PreparedStatement ps = conn.prepareStatement("UPDATE PROJECT_X_EMPLOYEE SET FEEDBACK = ? WHERE ID_PROJECT = ? AND ID_EMPLOYEE = ?");
-        ps.setString(1, feedBack);
-        ps.setInt(2, idProject);
-        ps.setInt(3, idEmployee);
-        ps.executeUpdate();
-        close(ps);
-        close(conn);
-    }
-
     public ProjectXEmployeeTO searchPXEByPk(int pK) throws Exception {
         Connection conn = getConnection();
         ProjectXEmployeeTO projectXEmployeeTO = null;
-        PreparedStatement ps = conn.prepareStatement("SELECT ID,ID_PROJECT,ID_EMPLOYEE,HOURS_INVESTED,FEEDBACK FROM HTH.PROJECT_X_EMPLOYEE WHERE ID = ?");
+        PreparedStatement ps = conn.prepareStatement("SELECT ID,ID_PROJECT,ID_EMPLOYEE FROM HTH.PROJECT_X_EMPLOYEE WHERE ID = ?");
         ps.setInt(1, pK);
         ResultSet rs = ps.executeQuery();
         if (rs.next()) {
             int id = rs.getInt("id");
             int projectId = rs.getInt("id_project");
             int idEmployee = rs.getInt("id_employee");
-            int hoursInvested = rs.getInt("hours_invested");
-            String feedback = rs.getString("feedback");
-            projectXEmployeeTO = new ProjectXEmployeeTO(id, projectId, idEmployee, hoursInvested, feedback);
+            projectXEmployeeTO = new ProjectXEmployeeTO(id, projectId, idEmployee);
         }
         close(rs);
         close(ps);
@@ -160,7 +135,7 @@ public class ProjectXEmployeeService extends Service implements ICrud<ProjectXEm
     public List<ProjectXEmployeeTO> getProjectEmployeeById(int employeeId) throws Exception {
         Connection conn = getConnection();
         List<ProjectXEmployeeTO> projectXEmployeeList = new ArrayList<>();
-        PreparedStatement ps = conn.prepareStatement("SELECT pxe.ID, pxe.ID_PROJECT, p.name AS project_name, pxe.id_employee, pxe.hours_invested, pxe.feedback "
+        PreparedStatement ps = conn.prepareStatement("SELECT pxe.ID, pxe.ID_PROJECT, pxe.id_employee "
                 + "FROM project_x_employee pxe "
                 + "INNER JOIN project p ON pxe.id_project = p.id "
                 + "WHERE pxe.id_employee = ?");
@@ -170,12 +145,9 @@ public class ProjectXEmployeeService extends Service implements ICrud<ProjectXEm
         while (rs.next()) {
             int id = rs.getInt("id");
             int projectId = rs.getInt("id_project");
-            String projectName = rs.getString("project_name");
             int idEmployee = rs.getInt("id_employee");
-            int hoursInvested = rs.getInt("hours_invested");
-            String feedback = rs.getString("feedback");
 
-            ProjectXEmployeeTO projectXEmployeeTO = new ProjectXEmployeeTO(id, projectId, idEmployee, hoursInvested, feedback, projectName);
+            ProjectXEmployeeTO projectXEmployeeTO = new ProjectXEmployeeTO(id, projectId, idEmployee);
             projectXEmployeeList.add(projectXEmployeeTO);
         }
 
@@ -184,33 +156,6 @@ public class ProjectXEmployeeService extends Service implements ICrud<ProjectXEm
         close(conn);
 
         return projectXEmployeeList;
-    }
-
-    public List<ProjectXEmployeeTO> getAllProjectsXEmployeesWithNames() throws Exception {
-        Connection conn = getConnection();
-        List<ProjectXEmployeeTO> projectEmployeeList = new ArrayList<>();
-        PreparedStatement ps = conn.prepareStatement("SELECT pxe.ID, p.name AS project_name, e.first_name AS employee_name, pxe.HOURS_INVESTED, pxe.FEEDBACK "
-                + "FROM HTH.PROJECT_X_EMPLOYEE pxe "
-                + "INNER JOIN HTH.PROJECT p ON pxe.ID_PROJECT = p.ID "
-                + "INNER JOIN HTH.EMPLOYEE e ON pxe.ID_EMPLOYEE = e.ID");
-        ResultSet rs = ps.executeQuery();
-
-        while (rs.next()) {
-            int id = rs.getInt("ID");
-            String projectName = rs.getString("project_name");
-            String employeeName = rs.getString("employee_name");
-            int hoursInvested = rs.getInt("HOURS_INVESTED");
-            String feedback = rs.getString("FEEDBACK");
-
-            ProjectXEmployeeTO projectEmployeeTO = new ProjectXEmployeeTO(id, hoursInvested, feedback, projectName, employeeName);
-            projectEmployeeList.add(projectEmployeeTO);
-        }
-
-        close(rs);
-        close(ps);
-        close(conn);
-
-        return projectEmployeeList;
     }
 
 }
--- src/main/java/com/mycompany/edu/ulatina/hth_db_connetion/ProjectXEmployeeTO.java
+++ src/main/java/com/mycompany/edu/ulatina/hth_db_connetion/ProjectXEmployeeTO.java
@@ -8,53 +8,14 @@ public class ProjectXEmployeeTO implements Serializable {
     private int id;
     private int idProject;
     private int idEmployee;
-    private int hoursInvested;
-    private String feedBack;
-    private String projectName;
-    private String employeeName;
 
     public ProjectXEmployeeTO() {
     }
 
-    public ProjectXEmployeeTO(int id, int idProject, int idEmployee, int hoursInvested, String feedBack) {
+    public ProjectXEmployeeTO(int id, int idProject, int idEmployee) {
         this.id = id;
         this.idProject = idProject;
         this.idEmployee = idEmployee;
-        this.hoursInvested = hoursInvested;
-        this.feedBack = feedBack;
-    }
-
-    public ProjectXEmployeeTO(int id, int idProject, int idEmployee, int hoursInvested, String feedBack, String projectName) {
-        this.id = id;
-        this.idProject = idProject;
-        this.idEmployee = idEmployee;
-        this.hoursInvested = hoursInvested;
-        this.feedBack = feedBack;
-        this.projectName = projectName;
-    }
-
-    public ProjectXEmployeeTO(int id, int hoursInvested, String feedBack, String projectName, String employeeName) {
-        this.id = id;
-        this.hoursInvested = hoursInvested;
-        this.feedBack = feedBack;
-        this.projectName = projectName;
-        this.employeeName = employeeName;
-    }
-
-    public String getEmployeeName() {
-        return employeeName;
-    }
-
-    public void setEmployeeName(String employeeName) {
-        this.employeeName = employeeName;
-    }
-
-    public String getProjectName() {
-        return projectName;
-    }
-
-    public void setProjectName(String projectName) {
-        this.projectName = projectName;
     }
 
     public int getId() {
@@ -79,22 +40,6 @@ public class ProjectXEmployeeTO implements Serializable {
 
     public void setIdEmployee(int idEmployee) {
         this.idEmployee = idEmployee;
-    }
-
-    public int getHoursInvested() {
-        return hoursInvested;
-    }
-
-    public void setHoursInvested(int hoursInvested) {
-        this.hoursInvested = hoursInvested;
-    }
-
-    public String getFeedBack() {
-        return feedBack;
-    }
-
-    public void setFeedBack(String feedBack) {
-        this.feedBack = feedBack;
     }
 
 }
